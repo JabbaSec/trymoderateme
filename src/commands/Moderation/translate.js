2ca0eeb01a7995f8154d69f3ec1790b32b10b4b8
JabbaSec <68778279+JabbaSec@users.noreply.github.com>
2023-07-02 16:56:10 +0100

    Added translate module

---
+++ src/commands/Moderation/translate.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("translate")
+    .setDescription("Translate a text")
+    .addStringOption((option) =>
+      option
+        .setName("text")
+        .setDescription("The text to translate")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const text = interaction.options.getString("text");
+    const translatedText = await translate(text);
+    await interaction.editReply({
+      content: `Translated text: ${translatedText}`,
+    });
+  },
+};
+
+async function translate(text) {
+  const response = await axios.post("https://libretranslate.de/translate", {
+    q: text,
+    source: "auto",
+    target: "en",
+  });
+
+  return response.data.translatedText;
+}
