b7f5dcacef40c9ebc2b56d8b099ffc99614f08ea
JabbaSec <68778279+JabbaSec@users.noreply.github.com>
2024-02-22 14:01:25 +0000

    Added svg support for rooms

--- src/components/modals/announce.js
+++ src/components/modals/announce.js
@@ -1,5 +1,8 @@
 const axios = require("axios");
-const { EmbedBuilder } = require("discord.js");
+const { EmbedBuilder, AttachmentBuilder } = require("discord.js");
+const sharp = require("sharp");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
   data: {
@@ -10,24 +13,53 @@ module.exports = {
 
     axios
       .get(`https://tryhackme.com/api/room/details?codes=${roomCode}`)
-      .then((response) => {
+      .then(async (response) => {
         const roomDescription = response.data[roomCode].description;
         const roomFree = response.data[roomCode].freeToUse
           ? "Free Room!"
           : "Subscriber Only.";
+        let imageUrl = response.data[roomCode].image;
+
+        let imageAttachment;
+
+        if (imageUrl.endsWith(".svg")) {
+          try {
+            const imageResponse = await axios.get(imageUrl, {
+              responseType: "arraybuffer",
+            });
+            const svgBuffer = Buffer.from(imageResponse.data, "utf-8");
+
+            const pngBuffer = await sharp(svgBuffer).png().toBuffer();
+
+            const pngFilePath = path.join(__dirname, "room.png");
+            await fs.promises.writeFile(pngFilePath, pngBuffer);
+
+            imageAttachment = new AttachmentBuilder(pngFilePath, {
+              name: "room.png",
+            });
+          } catch (error) {
+            console.error("Error converting SVG to PNG: ", error);
+          }
+        }
 
         const announceEmbed = new EmbedBuilder()
           .setTitle(`${response.data[roomCode].title}`)
           .setDescription(`${roomDescription}`)
           .setColor("#000000")
-          .setImage(response.data[roomCode].image)
           .setFooter({ text: `${roomFree}` });
+
+        if (imageAttachment) {
+          announceEmbed.setImage(`attachment://room.png`);
+        } else {
+          announceEmbed.setImage(imageUrl);
+        }
 
         interaction.guild.channels.cache.get(process.env.ANNOUNCEMENTS).send({
           content: `${interaction.fields.getTextInputValue("modalTitle")}
           \nA new room has been released! Check it out: <https://tryhackme.com/jr/${roomCode}>
           \n<@&${process.env.ANNOUNCEMENT_ROLE_ID}>`,
           embeds: [announceEmbed],
+          ...(imageAttachment ? { files: [imageAttachment] } : {}),
         });
       })
       .catch((error) => {
